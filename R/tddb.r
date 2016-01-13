e69374251787c4179eecc68d4a406fd5a641fce5
KDB <KDB2@users.noreply.github.com>
2016-01-13 14:45:49 +0100

    Bump to version 0.9

--- R/tddb.r
+++ R/tddb.r
@@ -5,11 +5,11 @@
 ###                                                                          ###
 ###       PACKAGE NAME        amsReliability                                 ###
 ###       MODULE NAME         tddb.r                                         ###
-###       VERSION             0.8                                            ###
+###       VERSION             0.9                                            ###
 ###                                                                          ###
 ###       AUTHOR              Emmanuel Chery                                 ###
 ###       MAIL                emmanuel.chery@ams.com                         ###
-###       DATE                2015/11/10                                     ###
+###       DATE                2016/01/13                                     ###
 ###       PLATFORM            Windows 7 & Gnu/Linux 3.16                     ###
 ###       R VERSION           R 3.1.1                                        ###
 ###       REQUIRED PACKAGES   ggplot2, grid, MASS, nlstools, scales          ###
